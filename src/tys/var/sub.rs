3f886107e97b8af09db1891e457ccf6f0ad8057f
Arav K. <8qzqr58nz4x89lqg@bal-e.org>
2023-11-30 14:52:46 +0100

    [tys/var] impl 'Subtyping' for 'ScalarType'

--- src/tys/var/sub.rs
+++ src/tys/var/sub.rs
@@ -1,5 +1,61 @@
 use super::{super::{Subtyping, BoundResult, BoundError}, *};
 
+impl Subtyping for ScalarType {
+    fn unify_min(lhs: Self, rhs: Self) -> BoundResult<Self> {
+        match (lhs, rhs) {
+            (Self::Min, _) | (_, Self::Min) =>
+                BoundResult { error: None, inner: Self::Min },
+            (Self::Max, x) | (x, Self::Max) =>
+                BoundResult { error: None, inner: x },
+            (Self::Any, x) | (x, Self::Any) =>
+                BoundResult { error: None, inner: x },
+            (Self::Int(l), Self::Int(r)) => {
+                let result = Subtyping::unify_min(l, r);
+                BoundResult {
+                    error: result.error,
+                    inner: Self::Int(result.inner),
+                }
+            },
+        }
+    }
+
+    fn unify_max(lhs: Self, rhs: Self) -> BoundResult<Self> {
+        match (lhs, rhs) {
+            (Self::Min, x) | (x, Self::Min) =>
+                BoundResult { error: None, inner: x },
+            (Self::Max, _) | (_, Self::Max) =>
+                BoundResult { error: None, inner: Self::Max },
+            (Self::Any, x) | (x, Self::Any) =>
+                BoundResult { error: None, inner: x },
+            (Self::Int(l), Self::Int(r)) => {
+                let result = Subtyping::unify_max(l, r);
+                BoundResult {
+                    error: result.error,
+                    inner: Self::Int(result.inner),
+                }
+            },
+        }
+    }
+
+    fn infer(self, sup: Self) -> BoundResult<Self> {
+        match (self, sup) {
+            (x@Self::Min, _) | (x, Self::Max) =>
+                BoundResult { error: None, inner: x },
+            (Self::Max, x) | (_, x@Self::Min) =>
+                BoundResult { error: Some(BoundError), inner: x },
+            (Self::Any, x) | (x, Self::Any) =>
+                BoundResult { error: None, inner: x },
+            (Self::Int(l), Self::Int(r)) => {
+                let result = Subtyping::infer(l, r);
+                BoundResult {
+                    error: result.error,
+                    inner: Self::Int(result.inner),
+                }
+            },
+        }
+    }
+}
+
 impl Subtyping for IntType {
     fn unify_min(lhs: Self, rhs: Self) -> BoundResult<Self> {
         match (lhs, rhs) {
